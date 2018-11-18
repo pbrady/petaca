d32f646c2a60c66404cabc61e09680399be751de
ncarlson <neil.n.carlson@gmail.com>
2018-11-17 17:31:57 -0700

    Updates for flang
    CMakeLists.txt updates for flang. It also drops the ICE "workaround"
    that removes the default initialization of the parameter_list%params
    component, as this will almost certainly result in run time errors
    down the line.  Add the compiler option "-DFLANG_618" to restore it.

--- src/parameter_list_type.F90
+++ src/parameter_list_type.F90
@@ -172,7 +172,7 @@ module parameter_list_type
   type, extends(parameter_entry), public :: parameter_list
     private
     character(:), allocatable :: name_
-#ifdef FLANG_240
+#ifdef FLANG_618
     !! Omit default initialization to avoid compile error, but likely to cause run-time errors
     type(map_any) :: params! = map_any()
 #else
